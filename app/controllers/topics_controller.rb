42b21e2c3e1f743373530054e06a134cf1a1d69c
Yuankun Zhang <fmcocoa@gmail.com>
2016-10-17 01:35:34 +0800

    topics methods

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,33 @@
 class TopicsController < ApplicationController
   def new
     @topic = Topic.new
-    render :show_form
+    render :new
+  end
+
+  def create
+    @topic = Topic.find(params[:id])
+    @topic.assign_attributes(topic_params)
+
+    if @topic.save
+      @topics = Topic.all
+      render 'show'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:subject, :description, :created_at)
   end
 end
