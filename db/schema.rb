353b84c17b154320d09ce1449d56631b31ab60b4
Yuankun Zhang <fmcocoa@gmail.com>
2016-10-22 00:31:01 +0800

    tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019155736) do
+ActiveRecord::Schema.define(version: 20161021163039) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "subject"
