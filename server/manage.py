ed3e0df6cdc3c96fd5e3e4767473153030745221
Yuankun Zhang <fmcocoa@gmail.com>
2016-12-10 16:01:46 +0800

    manage.py

--- server/manage.py
+++ server/manage.py
@@ -1,15 +1,17 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 from flask import current_app
 from flask_script import Manager, prompt_bool
 
 from app import create_app
+from app.extensions import db
+from app.models import Topic, User, Vote
 
 manager = Manager(create_app)
 
 
 @manager.shell
 def make_shell_context():
-    return dict(app=current_app)
+    return dict(app=current_app, db=db)
 
 
 @manager.command
@@ -18,13 +20,26 @@ def db_init():
             'Caution!!!\nThis will drop the whole database '
             'and re-create it, are you sure? [yn]'):
         return
-    pass
+
+    db.drop_all()
+    db.create_all()
 
 
 @manager.command
 def dev_setup():
     """Setup a development environment"""
-    pass
+
+    db_init()
+
+
+@manager.command
+def about():
+    print 'Models:'
+    print ', '.join([
+        Topic.__name__,
+        User.__name__,
+        Vote.__name__,
+    ])
 
 
 if __name__ == '__main__':
